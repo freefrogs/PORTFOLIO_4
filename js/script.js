95b18ba05238f92ff01ef940ec692c89172e6847
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-02-27 13:23:29 +0100

    Add script, html & style for slide/up map

--- js/script.js
+++ js/script.js
@@ -1,48 +1,54 @@
-//Scroll to ID
-$('a[href^="#1"]').on('click', function (e) {
-	e.preventDefault();
+$(document).ready(function() {
+	//Scroll to ID
+	$('a[href^="#1"]').on('click', function (e) {
+		e.preventDefault();
 
-	$('html, body').animate({
-		scrollTop: $($(this).attr('href')).offset().top
-	}, 600);
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 600);
+	});
+	$('a[href^="#2"]').on('click', function (e) {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1200);
+	});
+	$('a[href^="#3"]').on('click', function (e) {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1800);
+	});
+
+	//play video
+	var $bacgroundFilm = $('#film_background');
+	var $film = $('#film');
+
+	$bacgroundFilm.on('click', function() {
+		$bacgroundFilm.css('display', 'none');
+		$film.fadeIn('slow');
+		$film.trigger('play');
+	});
+
+	// header modal (click on phone button)
+	var $modal = $('#header_modal');
+	var $done = $('#done');
+
+	$('#btn_modal').on('click', function() {
+		$modal.fadeIn();
+	});
+	$('#btn_done').on('click', function() {
+		$done.fadeIn();
+	});
+	$done.click(function() {
+			$modal.fadeOut();
+			$done.fadeOut();
+	});
+
+	//map - slide up & down
+	$('#up_down').click(function() {
+		$('#map').slideToggle('slow');
+	});
 });
-$('a[href^="#2"]').on('click', function (e) {
-	e.preventDefault();
-
-	$('html, body').animate({
-		scrollTop: $($(this).attr('href')).offset().top
-	}, 1200);
-});
-$('a[href^="#3"]').on('click', function (e) {
-	e.preventDefault();
-
-	$('html, body').animate({
-		scrollTop: $($(this).attr('href')).offset().top
-	}, 1800);
-});
-
-//play video
-var $bacgroundFilm = $('#film_background');
-var $film = $('#film');
-
-$bacgroundFilm.on('click', function() {
-	$bacgroundFilm.css('display', 'none');
-	$film.fadeIn('slow');
-	$film.trigger('play');
-});
-
-// header modal (click on phone button)
-var $modal = $('#header_modal');
-var $done = $('#done');
-
-$('#btn_modal').on('click', function() {
-	$modal.fadeIn();
-});
-$('#btn_done').on('click', function() {
-	$done.fadeIn();
-});
-$done.click(function() {
-		$modal.fadeOut();
-		$done.fadeOut();
-});
-
