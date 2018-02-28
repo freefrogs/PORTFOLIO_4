e1329c893831080d3c7aa43097c340c9fc723a15
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-02-28 12:09:23 +0100

    Add slideshow with script, html & style

--- js/script.js
+++ js/script.js
@@ -42,13 +42,36 @@ $(document).ready(function() {
 	$('#btn_done').on('click', function() {
 		$done.fadeIn();
 	});
-	$done.click(function() {
+	/*$done.click(function() {
 			$modal.fadeOut();
 			$done.fadeOut();
-	});
+	});*/
 
 	//map - slide up & down
-	$('#up_down').click(function() {
+	var $upDown = $('#up_down');
+
+	$upDown.on('click', function() {
 		$('#map').slideToggle('slow');
 	});
+
+	//slideshow
+	var slideIndex = 0;
+	showSlides();
+
+	function showSlides() {
+		var i;
+		var slides = document.getElementsByClassName("slide");
+		var dots = document.getElementsByClassName("dot");
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";  
+		}
+		slideIndex++;
+		if (slideIndex > slides.length) {slideIndex = 1}    
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slides[slideIndex-1].style.display = "block";  
+		dots[slideIndex-1].className += " active";
+		setTimeout(showSlides, 5000);
+	}
 });
